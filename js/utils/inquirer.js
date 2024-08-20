3bdcc39c03a40c065daf5dbaafcda9b5d9f238a4
Grawrb <robmaxfield22@gmail.com>
2024-08-20 14:18:23 -0600

    Added file seeds.sql. Added function 'menu' to inquirer.js to run inquirer questions

---
+++ js/utils/inquirer.js
@@ -0,0 +1,54 @@
+// Import inquirer and necessary queries
+const inquirer = require('inquirer');
+const queries = require('./queries.js');
+
+function menu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ]).then(answer => {
+        switch (answer.choice) {
+            case 'View all departments':
+                queries.viewDepartments();
+                break;
+            case 'View all roles':
+                queries.viewRoles();
+                break;
+            case 'View all employees':
+                queries.viewEmployees();
+                break;
+            case 'Add a department':
+                queries.addDepartment();
+                break;
+            case 'Add a role':
+                queries.addRole();
+                break;
+            case 'Add an employee':
+                queries.addEmployee();
+                break;
+            case 'Update an employee role':
+                queries.updateEmployeeRole();
+                break;
+            case 'Exit':
+                console.log('Goodbye!');
+                process.exit();
+        }
+    });
+}
+
+module.exports = {
+    menu
+};
